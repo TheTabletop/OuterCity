be4e1f6964b2a3efa6925bc77b2fd9211b8a2f6e
Alex <huang.alexh@gmail.com>
2017-05-02 21:09:04 -0500

    added tests for GroupProfile message button and profile link

--- tests/spec.js
+++ tests/spec.js
@@ -4,7 +4,7 @@ describe('User Profile Page', function () {
         browser.get('https://thetabletop.github.io/components/views/userProfile.html');
     });
 
-    it('should open a message popup', function () {
+    it('should open a message popup after clicking the message button.', function () {
         var showMessage = element(by.className('button button2'));
 
         showMessage.click();
@@ -18,5 +18,27 @@ describe('User Profile Page', function () {
        noticeBoard.click();
 
        expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/groupProfile.html');
+    });
+});
+
+describe('Group Profile Page', function () {
+    beforeEach(function () {
+        browser.get('https://thetabletop.github.io/components/views/groupProfile.html');
+    });
+
+    it('should open a message popup after clicking the message button.', function () {
+        var showMessage = element(by.className('button button2'));
+
+        showMessage.click();
+
+        expect(showMessage.evaluate('popups.showMessagbox')).toEqual(true);
+    });
+
+    it('sends you to the user\'s profile, after clicking on their profile link.', function () {
+        var profileLink = element(by.linkText('Profile'));
+
+        profileLink.click();
+
+        expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/userProfile.html');
     });
 });
