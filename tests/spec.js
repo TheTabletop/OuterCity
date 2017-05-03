9349be3f03d5d5788197d033c4fed65f35422a76
Alex <huang.alexh@gmail.com>
2017-05-02 20:57:02 -0500

    Added tests for UserProfile message button and notice board link

--- tests/spec.js
+++ tests/spec.js
@@ -1,36 +1,22 @@
 // spec.js
-describe('User Profile Page', function() {
-  beforeEach(function() {
-    browser.get('https://thetabletop.github.io/components/views/userProfile.html');
-  });
+describe('User Profile Page', function () {
+    beforeEach(function () {
+        browser.get('https://thetabletop.github.io/components/views/userProfile.html');
+    });
 
+    it('should open a message popup', function () {
+        var showMessage = element(by.className('button button2'));
 
-  it('should send you to the userWall after clicking View Full Profile', function() {
-    element(by.buttonText('View Full Profile')).click();
-    expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/userWall.html');
-  });
+        showMessage.click();
 
+        expect(showMessage.evaluate('popups.showMessagbox')).toEqual(true);
+    });
 
-  it('should send you to the inbox after clicking the Message button', function() {
-    element(by.buttonText('Message')).click();
-    expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/inbox.html');
-  });
-});
+    it('sends you to group profile after clicking, \'Visit their Notice Board!\'', function () {
+       var noticeBoard = element(by.linkText('Visit their Notice Board!'));
 
-describe('Group Profile Page', function() {
-  beforeEach(function() {
-    browser.get('https://thetabletop.github.io/components/views/groupProfile.html');
-  });
+       noticeBoard.click();
 
-
-  it('should send you to the groupWall after clicking View Full Profile', function() {
-    element(by.buttonText('View Full Profile')).click();
-    expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/groupWall.html');
-  });
-
-
-  it('should send you to the inbox after clicking the Message button', function() {
-    element(by.buttonText('Message')).click();
-    expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/inbox.html');
-  });
+       expect(browser.getCurrentUrl()).toBe('https://thetabletop.github.io/components/views/groupProfile.html');
+    });
 });
